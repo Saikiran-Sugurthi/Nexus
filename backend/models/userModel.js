a6775ca5bf435314be82df14d6fbadd0041cfb0e
Saikiran-Sugurthi <saikiranytg1@gmail.com>
2025-08-02 23:55:36 +0530

    day01

---
+++ backend/models/userModel.js
@@ -0,0 +1,17 @@
+const mongoose=require("mongoose");
+
+
+const userSchema=new mongoose.Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true,unique:true},
+    password:{type:String,required:true},
+    pic:{type:String,required:true}
+},{timestamps:true});
+
+userSchema.pre("save",async function (next) {
+
+    if(!this.isModified) return next();
+
+    
+    
+})
